da9c3bdee510f35895b0434f0d2314a1b3208436
unknown <kcperki2@illinois.edu>
2015-12-04 10:53:14 -0600

    Initial Sprites, Various brushes

--- Color_Mapper.sv
+++ Color_Mapper.sv
@@ -13,16 +13,20 @@
 //-------------------------------------------------------------------------
 
 
-module  color_mapper ( input        [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
-								input				left_btn, Clk, vgaClk,
+module  color_mapper ( input        [9:0] BallX, BallY, DrawX, DrawY,
+								input				left_btn, vgaClk,
 								input [1:0]		ramOut, ramOut2,
-								output logic ram_read, ram_write,
+								input [2:0]		romOut,
+								output logic ram_read, ram_write, rom_read,
 								output logic [1:0] ramIn, ramIn2,
                        output logic [7:0]  Red, Green, Blue );
     
 	 logic [9:0] bX, bY;
 	 logic canvas, tools;
 	 logic [3:0] curColor;
+	 logic [9:0] Brush_size;
+	 logic [1:0] Brush_type;
+	 logic pressed;
 	 
 	 
  /* Old Ball: Generated square box by checking if the current pixel is within a square of length
@@ -38,24 +42,25 @@ module  color_mapper ( input        [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
      of the 12 available multipliers on the chip!  Since the multiplicants are required to be signed,
 	  we have to first cast them from logic to int (signed by default) before they are multiplied). */
 	  
-    int DistX, DistY, Size;
+    int DistX, DistY, DistX2, DistY2, Size;
 	 assign DistX = DrawX - BallX;
     assign DistY = DrawY - BallY;
-    assign Size = Ball_size;
+	 assign DistX2 = DrawX > BallX ? DrawX - BallX : BallX - DrawX;
+	 assign DistY2 = DrawY > BallY ? DrawY - BallY : BallY - DrawY;
+    assign Size = Brush_size;
 	 assign bX = DrawX - 100;
 	 assign bY = DrawY - 100;
 	 assign ram_read = canvas || DrawX == 99;
 	 assign ram_write = canvas;
+	 assign rom_read = tools || DrawX == 554;
 	 
 	 always_comb
 	 begin
 			if (DrawX < 540 && DrawY < 380 && DrawX >= 100 && DrawY >= 100)
-			begin
 				canvas = 1'b1;
-			end
 			else
 				canvas = 1'b0;
-			if (DrawX < 625 && DrawY < 235 && DrawX >= 555 && DrawY >= 100)
+			if (DrawX < 625 && DrawY < 270 && DrawX >= 555 && DrawY >= 100)
 				tools = 1'b1;
 			else
 				tools = 1'b0;
@@ -66,46 +71,81 @@ module  color_mapper ( input        [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
 	  begin
 			ramIn <= ramOut;
 			ramIn2 <= ramOut2;
-			if ( ( DistX*DistX + DistY*DistY) <= (Size * Size) ) 
+			if (left_btn)
+			begin
+				// First set of 8 colors
+				if (BallX < 85 && BallX >= 55 && BallY < 130 && BallY >= 100)
+					curColor <= 4'b0000;
+				else if (BallX < 85 && BallX >= 55 && BallY < 165 && BallY >= 135)
+					curColor <= 4'b0001;
+				else if (BallX < 45 && BallX >= 15 && BallY < 130 && BallY >= 100)
+					curColor <= 4'b0010;
+				else if (BallX < 45 && BallX >= 15 && BallY < 165 && BallY >= 135)
+					curColor <= 4'b0011;
+				else if (BallX < 85 && BallX >= 55 && BallY < 200 && BallY >= 170)
+					curColor <= 4'b0100;
+				else if (BallX < 85 && BallX >= 55 && BallY < 235 && BallY >= 205)
+					curColor <= 4'b0101;
+				else if (BallX < 45 && BallX >= 15 && BallY < 200 && BallY >= 170)
+					curColor <= 4'b0110;
+				else if (BallX < 45 && BallX >= 15 && BallY < 235 && BallY >= 205)
+					curColor <= 4'b0111;
+				// Second set of 8 colors
+				else if (BallX < 85 && BallX >= 55 && BallY < 270 && BallY >= 240)
+					curColor <= 4'b1000;
+				else if (BallX < 85 && BallX >= 55 && BallY < 305 && BallY >= 275)
+					curColor <= 4'b1001;
+				else if (BallX < 45 && BallX >= 15 && BallY < 270 && BallY >= 240)
+					curColor <= 4'b1010;
+				else if (BallX < 45 && BallX >= 15 && BallY < 305 && BallY >= 275)
+					curColor <= 4'b1011;
+				else if (BallX < 85 && BallX >= 55 && BallY < 340 && BallY >= 310)
+					curColor <= 4'b1100;
+				else if (BallX < 85 && BallX >= 55 && BallY < 375 && BallY >= 345)
+					curColor <= 4'b1101;
+				else if (BallX < 45 && BallX >= 15 && BallY < 340 && BallY >= 310)
+					curColor <= 4'b1110;
+				else if (BallX < 45 && BallX >= 15 && BallY < 375 && BallY >= 345)
+					curColor <= 4'b1111;
+				// Larger Brush Size Button
+				else if (BallX < 585 && BallX >= 555 && BallY < 130 && BallY >= 100 && !pressed)
+				begin
+					if (Brush_size < 10'h0ff)
+						Brush_size <= Brush_size + 'b11;
+					pressed <= 1'b1;
+				end
+				// Smaller Brush Size Button
+				else if (BallX < 625 && BallX >= 595 && BallY < 130 && BallY >= 100 && !pressed)
+				begin
+					if (Brush_size > 10'b0)
+						Brush_size <= Brush_size - 'b11;
+					pressed <= 1'b1;
+				end
+				// Circle Brush
+				else if (BallX < 585 && BallX >= 555 && BallY < 165 && BallY >= 135)
+					Brush_type <= 2'b00;
+				// Sqaure Brush
+				else if (BallX < 625 && BallX >= 595 && BallY < 165 && BallY >= 135)
+					Brush_type <= 2'b01;
+				// Horizontal Line Brush
+				else if (BallX < 585 && BallX >= 555 && BallY < 200 && BallY >= 170)
+					Brush_type <= 2'b10;
+				// Vertical Line Brush
+				else if (BallX < 625 && BallX >= 595 && BallY < 200 && BallY >= 170)
+					Brush_type <= 2'b11;
+			end
+			else
+				pressed <= 1'b0;
+				
+			if ((Brush_type == 2'b00 &&( DistX*DistX + DistY*DistY) <= (Size * Size))
+				|| (Brush_type == 2'b01 && DistX2 <= Size && DistY2 <= Size)
+				|| (Brush_type == 2'b10 && DistY2 <= Size >> 3 && DistX2 <= Size)
+				|| (Brush_type == 2'b11 && DistX2 <= Size >> 3 && DistY2 <= Size))
 			begin 
-				if (left_btn)
+				if (left_btn && BallX < 540 && BallY < 380 && BallX >= 100 && BallY >= 100)
 				begin
 					ramIn <= curColor[1:0];
 					ramIn2 <= curColor[3:2];
-					if (DrawX < 85 && DrawX >= 55 && DrawY < 130 && DrawY >= 100)
-						curColor <= 4'b0000;
-					else if (DrawX < 85 && DrawX >= 55 && DrawY < 165 && DrawY >= 135)
-						curColor <= 4'b0001;
-					else if (DrawX < 45 && DrawX >= 15 && DrawY < 130 && DrawY >= 100)
-						curColor <= 4'b0010;
-					else if (DrawX < 45 && DrawX >= 15 && DrawY < 165 && DrawY >= 135)
-						curColor <= 4'b0011;
-					else if (DrawX < 85 && DrawX >= 55 && DrawY < 200 && DrawY >= 170)
-						curColor <= 4'b0100;
-					else if (DrawX < 85 && DrawX >= 55 && DrawY < 235 && DrawY >= 205)
-						curColor <= 4'b0101;
-					else if (DrawX < 45 && DrawX >= 15 && DrawY < 200 && DrawY >= 170)
-						curColor <= 4'b0110;
-					else if (DrawX < 45 && DrawX >= 15 && DrawY < 235 && DrawY >= 205)
-						curColor <= 4'b0111;
-						
-					else if (DrawX < 85 && DrawX >= 55 && DrawY < 270 && DrawY >= 240)
-						curColor <= 4'b1000;
-					else if (DrawX < 85 && DrawX >= 55 && DrawY < 305 && DrawY >= 275)
-						curColor <= 4'b1001;
-					else if (DrawX < 45 && DrawX >= 15 && DrawY < 270 && DrawY >= 240)
-						curColor <= 4'b1010;
-					else if (DrawX < 45 && DrawX >= 15 && DrawY < 305 && DrawY >= 275)
-						curColor <= 4'b1011;
-					else if (DrawX < 85 && DrawX >= 55 && DrawY < 340 && DrawY >= 310)
-						curColor <= 4'b1100;
-					else if (DrawX < 85 && DrawX >= 55 && DrawY < 375 && DrawY >= 345)
-						curColor <= 4'b1101;
-					else if (DrawX < 45 && DrawX >= 15 && DrawY < 340 && DrawY >= 310)
-						curColor <= 4'b1110;
-					else if (DrawX < 45 && DrawX >= 15 && DrawY < 375 && DrawY >= 345)
-						curColor <= 4'b1111;
-					
 				end
 				case (curColor)
 				4'b0000:
@@ -206,7 +246,8 @@ module  color_mapper ( input        [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
 						Blue <= 8'h00;
 					end
 				endcase
-        end
+			end
+			
 		  else if (DrawX < 100 || DrawX >= 540 || DrawY < 100 || DrawY >= 380)
 			begin
 				if (DrawX < 85 && DrawX >= 55 && DrawY < 130 && DrawY >= 100)
@@ -305,6 +346,42 @@ module  color_mapper ( input        [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
 					Red <= 8'haa;
 					Green <= 8'h55;
 					Blue <= 8'h00;
+				end
+				
+				else if (tools)
+				begin
+				case (romOut)
+				3'b000:
+					begin
+						Red <= 8'h88;
+						Green <= 8'h88;
+						Blue <= 8'h88;
+					end
+				3'b100:
+					begin
+						Red <= 8'h49;
+						Green <= 8'h49;
+						Blue <= 8'h49;
+					end
+				3'b111:
+					begin
+						Red <= 8'hff;
+						Green <= 8'hff;
+						Blue <= 8'hff;
+					end
+				3'b010:
+					begin
+						Red <= 8'h00;
+						Green <= 8'h00;
+						Blue <= 8'h00;
+					end
+				default:
+					begin
+						Red <= 8'h88;
+						Green <= 8'h88;
+						Blue <= 8'h88;
+					end
+				endcase
 				end
 				
 				else
@@ -437,6 +514,7 @@ module  color_mapper ( input        [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
 				end
 				endcase
 				end
+				
 		  end 
 		  if (BallX == DrawX)
 		  begin
--- lab8.sv
+++ lab8.sv
@@ -1,14 +1,3 @@
-//-------------------------------------------------------------------------
-//      lab7_usb.sv                                                      --
-//      Christine Chen                                                   --
-//      Fall 2014                                                        --
-//                                                                       --
-//      Fall 2014 Distribution                                           --
-//                                                                       --
-//      For use with ECE 385 Lab 7                                       --
-//      UIUC ECE Department                                              --
-//-------------------------------------------------------------------------
-
 
 module  lab8 		( input         CLOCK_50,
                        input[3:0]    KEY, //bit 0 is set up as Reset
@@ -25,85 +14,35 @@ module  lab8 		( input         CLOCK_50,
 												 VGA_BLANK_N,			//VGA Blank signal
 												 VGA_VS,					//VGA virtical sync signal	
 												 VGA_HS,					//VGA horizontal sync signal
-							  // CY7C67200 Interface
-							  inout [15:0]  OTG_DATA,						//	CY7C67200 Data bus 16 Bits
-							  output [1:0]  OTG_ADDR,						//	CY7C67200 Address 2 Bits
-							  output        OTG_CS_N,						//	CY7C67200 Chip Select
-												 OTG_RD_N,						//	CY7C67200 Write
-												 OTG_WR_N,						//	CY7C67200 Read
-												 OTG_RST_N,						//	CY7C67200 Reset
-							  input			 OTG_INT,						//	CY7C67200 Interrupt
-							  // SDRAM Interface for Nios II Software
-							  output [12:0] DRAM_ADDR,				// SDRAM Address 13 Bits
-							  inout [31:0]  DRAM_DQ,				// SDRAM Data 32 Bits
-							  output [1:0]  DRAM_BA,				// SDRAM Bank Address 2 Bits
-							  output [3:0]  DRAM_DQM,				// SDRAM Data Mast 4 Bits
-							  output			 DRAM_RAS_N,			// SDRAM Row Address Strobe
-							  output			 DRAM_CAS_N,			// SDRAM Column Address Strobe
-							  output			 DRAM_CKE,				// SDRAM Clock Enable
-							  output			 DRAM_WE_N,				// SDRAM Write Enable
-							  output			 DRAM_CS_N,				// SDRAM Chip Select
-							  output			 DRAM_CLK,				// SDRAM Clock
-							  inout			 PS2_DAT,
-							  inout			 PS2_CLK
+								// PS2 Interface
+							  inout			 PS2_DAT,				// PS2 Data Bus
+							  inout			 PS2_CLK					// PS2 Clock 
 							  
 											);
     
-    logic Reset_h, vssig, Clk, vgaClock, left_btn, middle_btn, right_btn;
-    logic [9:0] drawxsig, drawysig, ballsizesig, cursorX, cursorY;
-	 logic [15:0] keycode;
-	 logic [7:0] RPrev, GPrev, BPrev, byte1, byte2, byte3;
+    logic Reset_h, Clk, vgaClock, left_btn, middle_btn, right_btn, ram_re, ram_we, rom_re;
+    logic [9:0] drawxsig, drawysig, cursorX, cursorY;
+	 logic [9:0] xOffset, yOffset, xOffset2, yOffset2, xOffset3, yOffset3, xToolOffset;
+	 logic [1:0] ramIn, ramOut, ramIn2, ramOut2;
+	 logic [2:0] romOut;
+	 logic [17:0] ramAddrRead, ramAddrWrite;
+	 logic [13:0] romAddrRead;
     
 	 assign Clk = CLOCK_50;
     assign {Reset_h}=~ (KEY[0]);  // The push buttons are active low
 	 assign VGA_CLK = vgaClock;
-	
+	 assign xOffset = drawxsig-100;
+	 assign xOffset2 = (xOffset == 439) ? 0 : xOffset+1;
+	 assign xOffset3 = (xOffset == 0) ? 439 : xOffset-1;
+	 assign yOffset = drawysig-100;
+	 assign yOffset2 = (xOffset == 439) ? (yOffset == 279) ? 0 :  yOffset + 1 : yOffset;
+	 assign yOffset3 = (xOffset == 0) ? (yOffset == 0) ? 279 : yOffset - 1 : yOffset;
+	 assign xToolOffset = drawxsig-555;
+	 assign ramAddrRead = {xOffset2[8:0], yOffset2[8:0]};
+	 assign ramAddrWrite = {xOffset3[8:0], yOffset3[8:0]};
+	 assign romAddrRead  = (yOffset[8:0] * 70) + xToolOffset[8:0] + 'b1;
 	 
-	 wire [1:0] hpi_addr;
-	 wire [15:0] hpi_data_in, hpi_data_out;
-	 wire hpi_r, hpi_w,hpi_cs;
-	 
-	 hpi_io_intf hpi_io_inst(   .from_sw_address(hpi_addr),
-										 .from_sw_data_in(hpi_data_in),
-										 .from_sw_data_out(hpi_data_out),
-										 .from_sw_r(hpi_r),
-										 .from_sw_w(hpi_w),
-										 .from_sw_cs(hpi_cs),
-		 								 .OTG_DATA(OTG_DATA),    
-										 .OTG_ADDR(OTG_ADDR),    
-										 .OTG_RD_N(OTG_RD_N),    
-										 .OTG_WR_N(OTG_WR_N),    
-										 .OTG_CS_N(OTG_CS_N),    
-										 .OTG_RST_N(OTG_RST_N),   
-										 .OTG_INT(OTG_INT),
-										 .Clk(Clk),
-										 .Reset(Reset_h)
-	 );
-	 
-	 //The connections for nios_system might be named different depending on how you set up Qsys
-	 nios_system nios_system(
-										 .clk_clk(Clk),         
-										 .reset_reset_n(KEY[0]),   
-										 .sdram_wire_addr(DRAM_ADDR), 
-										 .sdram_wire_ba(DRAM_BA),   
-										 .sdram_wire_cas_n(DRAM_CAS_N),
-										 .sdram_wire_cke(DRAM_CKE),  
-										 .sdram_wire_cs_n(DRAM_CS_N), 
-										 .sdram_wire_dq(DRAM_DQ),   
-										 .sdram_wire_dqm(DRAM_DQM),  
-										 .sdram_wire_ras_n(DRAM_RAS_N),
-										 .sdram_wire_we_n(DRAM_WE_N), 
-										 .sdram_clk_clk(DRAM_CLK),
-										 .keycode_export(keycode),  
-										 .otg_hpi_address_export(hpi_addr),
-										 .otg_hpi_data_in_port(hpi_data_in),
-										 .otg_hpi_data_out_port(hpi_data_out),
-										 .otg_hpi_cs_export(hpi_cs),
-										 .otg_hpi_r_export(hpi_r),
-										 .otg_hpi_w_export(hpi_w),
-										 );
-	
-	//Fill in the connections for the rest of the modules 
+
     vga_controller vgasync_instance(.Clk(Clk), .Reset(Reset_h),
 												.hs(VGA_HS), .vs(VGA_VS),
 												.pixel_clk(vgaClock), .blank(VGA_BLANK_N),
@@ -111,39 +50,35 @@ module  lab8 		( input         CLOCK_50,
 												.DrawX(drawxsig), .DrawY(drawysig)
 												);
 												
-	PS2_Demo 	ps2 (.CLOCK_50(Clk), .KEY(KEY), .PS2_CLK(PS2_CLK), .PS2_DAT(PS2_DAT), 
+	Mouse 	cursor_0 (.CLOCK_50(Clk), .KEY(KEY), .PS2_CLK(PS2_CLK), .PS2_DAT(PS2_DAT), 
 							.HEX0(HEX0), .HEX1(HEX1), .HEX2(HEX2),
 							.HEX3(HEX3), .HEX4(HEX4), .HEX5(HEX5),
-							.byte1(byte1), .byte2(byte2), .byte3(byte3));
-   /*
-    ball ball_instance(.Reset(Reset_h), .frame_clk(VGA_VS),
-								.keycode(keycode[7:0]),
-								.BallX(ballxsig), .BallY(ballysig), .BallS(ballsizesig)
-								);
-   */
-	
-	 cursor cursor0 (.Clk(Clk), .Reset(Reset_h), 
-						.byte1(byte1), .byte2(byte2), .byte3(byte3),
-						.cursorX(cursorX), .cursorY(cursorY), 
-						.leftButton(left_btn), .middleButton(middle_btn), .rightButton(right_btn));
-	
-	 /*
-	 frameSave	frame_0 (.xPos(drawxsig), .yPos(drawxsig), .Clk(vgaClock),
-								.Ri(VGA_R), .Gi(VGA_G), .Bi(VGA_B), 
-								.Ro(RPrev), .Go(GPrev), .Bo(BPrev));
-	*/
+							.cursorX(cursorX), .cursorY(cursorY), 
+							.leftButton(left_btn), .middleButton(middle_btn), .rightButton(right_btn));
+
     color_mapper color_instance(.BallX(cursorX), .BallY(cursorY),
-											.DrawX(drawxsig), .DrawY(drawysig), .Ball_size(10'b0100),
-											.left_btn(left_btn),
-											//.RedPrev(RPrev), .GreenPrev(GPrev), .BluePrev(BPrev),
+											.DrawX(drawxsig), .DrawY(drawysig),
+											.left_btn(left_btn), .vgaClk(vgaClock),
+											.ramIn(ramIn), .ramIn2(ramIn2), 
+											.ramOut(ramOut), .ramOut2(ramOut2),
+											.romOut(romOut),
+											.ram_read(ram_re), .ram_write(ram_we), .rom_read(rom_re),
 											.Red(VGA_R), .Green(VGA_G), .Blue(VGA_B) );
+											
+	ram ram_0 ( .data(ramIn),
+					.rdaddress(ramAddrRead), .rdclock(~vgaClock), .rden(ram_re), 
+					.wraddress(ramAddrWrite), .wrclock(~vgaClock), .wren(ram_we), 
+					.q(ramOut));
+	ram ram_1 ( .data(ramIn2),
+					.rdaddress(ramAddrRead), .rdclock(~vgaClock), .rden(ram_re), 
+					.wraddress(ramAddrWrite), .wrclock(~vgaClock), .wren(ram_we), 
+					.q(ramOut2));
+	/*				
+	rom rom_1 ( .address(romAddrRead), .clock(~vgaClock), .rden(rom_re),
+					.q(romOut));
+	*/
+	rom2 rom_1 ( .address(romAddrRead), .clock(~vgaClock), .rden(rom_re),
+					.q(romOut));
 										  
     
-
-	 /**************************************************************************************
-	    ATTENTION! Please answer the following quesiton in your lab report! Points will be allocated for the answers!
-		 Hidden Question #1/2:
-          What are the advantages and/or disadvantages of using a USB interface over PS/2 interface to
-			 connect to the keyboard? List any two.  Give an answer in your Post-Lab.
-     **************************************************************************************/
 endmodule
